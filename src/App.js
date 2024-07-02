2676ec1409565e3576f9dd4bf70ad0d69fe9259a
huynhanhkhoa <huynhanhkhoait@gmail.com>
2024-07-02 09:19:53 +0700

    6

--- src/App.js
+++ src/App.js
@@ -2,7 +2,17 @@ import React, { useState } from 'react';
 import './App.css';
 import MaterialTable from 'material-table'
 import jsonFile from "./temp.json";
+import ReactDOM from 'react-dom';
 
+var password = "VinhHao";
+while (k != password) {
+var k = prompt("Nhập mật khẩu");
+if(k==password){alert('Mật khẩu đúng, nhấn Ok để tiếp tục')}
+else if (k != password) {
+  alert("Sai mật khẩu! Lêu lêu");
+  }
+}
+  
 function App() {
   const [tableData, setTableData] = useState(
     jsonFile
@@ -21,40 +31,26 @@ function App() {
     { title: "Shapiro-Wilk test", field: "Shapiro-Wilk test"},
     { title: "Cp", field: "Cp"},
     { title: "Cpk", field: "Cpk"},
-    { title: "%retention", field: "retention"},
-    { title: "%Reduction_da_tru", field: "Reduction_da_tru"},
-    { title: "%Reduction_chua_tru", field: "Reduction_chua_tru"}
+    { title: "%retention", field: "%Retention"},
+    { title: "%Reduction_da_tru", field: "%Reduction_da_tru"},
+    { title: "%Reduction_chua_tru", field: "%Reduction_chua_tru"}
   ]
+  
+
+
   return (
+    
     <div className="App">
       <div className='abc'>
         <h1 align="center">Shelf-life report - QA MCH </h1>
         <h4 align='center'>By Huynh Anh Khoa</h4>
       </div>
       <MaterialTable columns={columns} data={tableData}
-        editable={{
-          onRowAdd: (newRow) => new Promise((resolve, reject) => {
-            setTableData([...tableData, newRow])
-            setTimeout(() => resolve(), 500)
-          }),
-          onRowUpdate: (newRow, oldRow) => new Promise((resolve, reject) => {
-            const updatedData = [...tableData]
-            updatedData[oldRow.tableData.id] = newRow
-            setTableData(updatedData)
-            setTimeout(() => resolve(), 500)
-          }),
-          onRowDelete: (selectedRow) => new Promise((resolve, reject) => {
-            const updatedData = [...tableData]
-            updatedData.splice(selectedRow.tableData.id, 1)
-            setTableData(updatedData)
-            setTimeout(() => resolve(), 1000)
-          })
-        }}  
         onSelectionChange={(selectedRows) => console.log(selectedRows)}
         options={{
           sorting: true, search: true,
           searchFieldAlignment: "right", searchAutoFocus: true, searchFieldVariant: "standard",
-          filtering: true, paging: true, pageSizeOptions: [2, 5, 10, 20, 25, 50, 100], pageSize: 50,
+          filtering: true, paging: true, pageSizeOptions: [25, 50, 100], pageSize: 50,
           paginationType: "stepped", showFirstLastPageButtons: false, paginationPosition: "both", exportButton: true,
           exportAllData: true, exportFileName: "TableData", addRowPosition: "first", actionsColumnIndex: -1, selection: true,
           showSelectAllCheckbox: false, showTextRowsSelected: false, selectionProps: rowData => ({
